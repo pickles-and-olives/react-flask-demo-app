4b8a6bd0c6d3cfa73e79c99d4817a2fea542f2e0
Melissa McFadzien <melissam@aucklandeye.co.nz>
2024-01-27 15:17:44 +1300

    Initial commit

---
+++ react-flask-demo/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0);
+  const API_URL = 'http://localhost:5000/api';
+
+  const fetchTime = async() => {
+    const request = await axios.get(`${API_URL}/time`)
+    console.log(request)
+    const response = request.data;
+    setCurrentTime(response.time)
+  }
+  useEffect(() => {
+  fetchTime();
+  }, [count]);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          The current time is {currentTime}.
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
